90d4cda74643608d857aaa33aa17ba6913539e16
mquellhorst <michiel@ooble.nl>
2021-09-29 00:39:04 +0200

    sample contracts & ChakraUI
    Added the Staker sample contract from scaffold-eth
    Added basic ChakraUI setup

---
+++ components/Chakra.tsx
@@ -0,0 +1,37 @@
+import {
+  ChakraProvider,
+  cookieStorageManager,
+  localStorageManager,
+} from '@chakra-ui/react'
+import { Dict } from '@chakra-ui/utils'
+import { GetServerSidePropsContext } from 'next'
+import React, { ReactNode } from 'react'
+
+interface ChakraProps {
+  cookies?: string
+  theme?: Dict<string>
+  children: ReactNode
+}
+
+export const Chakra = ({ cookies, theme, children }: ChakraProps) => (
+  <ChakraProvider
+    colorModeManager={
+      cookies ? cookieStorageManager(cookies) : localStorageManager
+    }
+    theme={theme}
+  >
+    {children}
+  </ChakraProvider>
+)
+
+export type ServerSideProps<T> = { props: T } | Promise<{ props: T }>
+
+export function getServerSideProps({
+  req,
+}: GetServerSidePropsContext): ServerSideProps<{ cookies?: string }> {
+  return {
+    props: {
+      cookies: req.headers.cookie ?? '',
+    },
+  }
+}
---
+++ tests/Staker.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('Staker contract', function () {
+  it("User can stake", async function () {
+    const Staker = await ethers.getContractFactory("Staker");
+  });
+});
