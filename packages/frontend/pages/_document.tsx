c9a27d28ff4d926f2d4302b9119e2fd1e86e9687
luxumbra <me@luxumbra.dev>
2021-10-10 21:55:16 +0100

    adding manifest and files with quality source file

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -17,7 +17,13 @@ class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          {/*  */}
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#00a300" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
